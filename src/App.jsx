b58af904ad89484838a33d1ec3e9bf3b5d3b708f
Antoine BANCTEL-CHEVREL <a.banctel.chevrel@gmail.com>
2025-02-03 21:45:50 +0100

    Open and close socket

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,53 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [wsUrl, setWsUrl] = useState(null)
+  const [socket, setSocket] = useState(null)
+  
+      function saveWsUrl(event) {
+
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const query = formData.get("query");
+        console.log(`WS Url '${query}'`);
+        setWsUrl(query);
+        console.log(wsUrl)
+
+      } 
+
+      function startTranscription() {
+        console.log("Starting transcription"); 
+        console.log(wsUrl)
+
+        const localSocket = new WebSocket(wsUrl);
+        setSocket(localSocket);
+      
+      }
+
+
+      function closeWS() {
+        console.log("Closing WS");  
+        console.log(socket)
+        socket.close();
+      }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Gladia Live Transcription Demo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <form onSubmit={saveWsUrl}>
+          <input name="query"  placeholder="WS URL" />
+          <button type="submit">Submit</button>
+        </form>
+
+        <button onClick={startTranscription} disabled={!wsUrl}>Open socket</button>
+
+        <button onClick={closeWS} disabled={!socket}>Close WS</button>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
